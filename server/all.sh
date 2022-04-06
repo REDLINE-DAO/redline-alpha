86c6443766ca6eda4ad6e76295f3e5d9f2218324
Corentin Derbré <LoeizD@users.noreply.github.com>
2022-04-07 01:45:20 +0800

    Update all.sh

--- server/all.sh
+++ server/all.sh
@@ -3,6 +3,7 @@
 echo "sending" $1 $2 $3
 source ~/cairo_venv/bin/activate
 export STARKNET_NETWORK=alpha-goerli
+export STARKNET_WALLET="starkware.starknet.wallets.open_zeppelin.OpenZeppelinAccount"
 echo "bef call" >> log_oracle.txt
 /root/cairo_venv/bin/starknet call --address $1 --abi ../cairo/artifacts/abis/oracles.json --function get_caller >> log_oracle.txt
 echo "after call" >> log_oracle.txt
