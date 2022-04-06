87b0c016c09e2b0435a752e3c46ddcf4580d31fb
Corentin Derbré <LoeizD@users.noreply.github.com>
2022-04-07 00:38:38 +0800

    debug

--- server/all.sh
+++ server/all.sh
@@ -3,6 +3,7 @@
 echo "sending" $1 $2 $3
 source ~/cairo_venv/bin/activate
 export STARKNET_NETWORK=alpha-goerli
+/root/cairo_venv/bin/starknet call --address $1 --abi ../cairo/artifacts/abis/oracles.json --function get_caller >> log_oracle.txt
 /root/cairo_venv/bin/starknet invoke \
     --address $1 \
     --abi ../cairo/artifacts/abis/oracles.json \
